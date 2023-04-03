2ec573823b339861bf4738263f334d3f8429a42d
harshwardhan sharma <hw.sharma9@gmail.com>
2023-04-03 21:04:17 +0530

    Post crud files uploaded
    Post crud files uploaded

---
+++ edit.php
@@ -0,0 +1,33 @@
+<form method="POST" action="#">
+  <!-- Title -->
+  <div>
+    <label class="block text-sm font-bold text-gray-700" for="title">
+      Title
+    </label>
+
+    <input
+      class="block w-full mt-1 border-gray-300 rounded-md shadow-sm placeholder:text-gray-400 placeholder:text-right focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+      type="text" name="email" placeholder="180" value="Create Laravel project" />
+  </div>
+
+  <!-- Description -->
+  <div class="mt-4">
+    <label class="block text-sm font-bold text-gray-700" for="password">
+      Description
+    </label> 
+    <textarea name="description"
+      class="block w-full mt-1 border-gray-300 rounded-md shadow-sm placeholder:text-gray-400 placeholder:text-right focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+      rows="4" placeholder="400">lorem ipsum dolor sit amet, consectet</textarea>
+  </div>
+
+  <div class="flex items-center justify-start mt-4 gap-x-2">
+    <button type="submit"
+      class="px-6 py-2 text-sm font-semibold rounded-md shadow-md text-sky-100 bg-sky-500 hover:bg-sky-700 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300">
+      Update
+    </button>
+    <a href=""
+      class="px-6 py-2 text-sm font-semibold text-gray-100 bg-gray-400 rounded-md shadow-md hover:bg-gray-600 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300">
+      Cancel
+    </a>
+  </div>
+</form>
---
+++ show.php
@@ -0,0 +1,15 @@
+<form method="POST" action="#">
+  <!-- Title -->
+  <div>
+    <h3 class="text-2xl font-semibold">Tailwind CSS 3 Post Show Title</h3>
+    <div class="flex items-center mb-4 space-x-2">
+      <span class="text-xs text-gray-500"> 20/12/22</span><span class="text-xs text-gray-500">Created by
+        Admin</span>
+    </div>
+    <p class="text-base text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+      aliquid numquam sequi! Commodi enim laborum illo quaerat illum quidem quam ea itaque in, nulla quis
+      vero qui cupiditate debitis. Ut. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quia
+      rerum qui autem vel? Veniam laborum omnis, delectus ab repellat labore tempora, id sit ipsum suscipit,
+      consectetur neque tenetur quam?</p>
+  </div>
+</form>
